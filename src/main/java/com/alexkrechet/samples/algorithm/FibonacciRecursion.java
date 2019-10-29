39a3fcd88d515bed569e17ef24a5d36f0ce5f2c8
Alexkrechet <rubassabaka88@gmail.com>
2019-10-29 20:40:59 +0200

    Fibonacci through recusive method was completed

--- src/main/java/com/alexkrechet/samples/algorithm/FibonacciRecursion.java
+++ src/main/java/com/alexkrechet/samples/algorithm/FibonacciRecursion.java
@@ -2,6 +2,7 @@ package com.alexkrechet.samples.algorithm;
 
 public class Fibonacci {
     public static void main (String [] arg){
-        
+        System.out.println(fibonacci(3));
     }
+    public static int fibonacci(int )
 }
